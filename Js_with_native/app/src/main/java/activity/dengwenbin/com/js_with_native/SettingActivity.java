8dbf2823874e00a94a1a6ace4e8835a348e745a9
dengDWB <402847229@qq.com>
2016-06-18 16:02:27 +0800

    add: 视图 Setting

--- Js_with_native/app/src/main/java/activity/dengwenbin/com/js_with_native/SettingActivity.java
+++ Js_with_native/app/src/main/java/activity/dengwenbin/com/js_with_native/SettingActivity.java
@@ -1,16 +1,47 @@
 package activity.dengwenbin.com.js_with_native;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 /**
  * Created by 40284 on 2016/6/18.
  */
 public class SettingActivity extends AppCompatActivity {
 
+    private WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+        webView = (WebView) findViewById(R.id.settingwebview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        webView.addJavascriptInterface(new JsInterfaceSetting(),"JsInterfaceSetting");
+        webView.setWebChromeClient(new WebChromeClient() {
+        });
+        webView.loadUrl("file:///android_asset/setting.html");
+    }
+
+    public class JsInterfaceSetting{
+        @JavascriptInterface
+        public void toMainActivity(){
+            Intent intent = new Intent(SettingActivity.this,MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+            finish();
+        }
+        @JavascriptInterface
+        public void toLoginActivity(){
+            Intent intent1 = new Intent(SettingActivity.this,LoginActivity.class);
+            intent1.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent1);
+            finish();
+        }
+
     }
 }
